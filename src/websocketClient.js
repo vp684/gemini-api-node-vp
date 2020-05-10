ffff243c0d6e53bc0d670bc1caabbe30abbda282
Viraj Patel <viraj84@gmail.com>
2020-05-10 16:18:32 -0400

    heartbeats default

--- src/websocketClient.js
+++ src/websocketClient.js
@@ -38,7 +38,8 @@ export default class GeminiAPIWebsocketClient {
   }
 
   openMarketSocket = (symbol, onOpen) => {
-    this.marketSocket = new WebSocket(`${this.baseUrl}/v1/marketdata/${symbol}`);
+    let opts = '?heartbeat=true'
+    this.marketSocket = new WebSocket(`${this.baseUrl}/v1/marketdata/${symbol}${opts}`);
     this.marketSocket.addEventListener(`open`, (...args) => {
       console.log(`Connected to market data WebSocket API`);
       if (typeof onOpen === `function`) onOpen(...args);
